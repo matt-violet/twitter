04e2a0a705485d4cdf306b51f17de1baa3d95f5e
mattviolet <mattviolet@gmail.com>
2020-11-25 15:26:08 -0800

    Add tweetbox to feed component

---
+++ src/TweetBox.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+
+function TweetBox() {
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src=""></Avatar>
+          <input placeholder="What's happening?" type="text"></input>
+        </div>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
+    </div>
+  )
+}
+
+export default TweetBox;
